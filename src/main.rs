e725d302ba06c9707e43d62710a473d8215d8a23
SillyBee <3576527475a@gmail.com>
2025-12-17 02:11:39 +0800

    feat: 单元测试与串口

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,48 @@
-
-
 #![no_std] // 不链接 Rust 标准库
 #![no_main] // 禁用所有 Rust 层级的入口点
 #![feature(custom_test_frameworks)]
 #![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
+mod serial;
 mod vga_buffer;
 
+//qemu内isa-debug-exit实现的推出
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
+}
+
+//单元测试
 #[cfg(test)]
 pub fn test_runner(tests: &[&dyn Fn()]) {
     println!("Running {} tests", tests.len());
     for test in tests {
         test();
     }
+    exit_qemu(QemuExitCode::Success);
 }
+
+#[test_case]
+fn trivial_assertion() {
+    print!("trivial assertion... ");
+    assert_eq!(1, 1);
+    println!("[ok]");
+}
+
+
 
 /// 这个函数将在 panic 时被调用
 #[panic_handler]
@@ -23,9 +51,15 @@ fn panic(info: &PanicInfo) -> ! {
     loop {}
 }
 
+
+
+
+
 #[unsafe(no_mangle)] // 不重整函数名
 pub extern "C" fn _start() -> ! {
     println!("ciallo");
-    panic!("Some panic message");
+
+    #[cfg(test)]
+    test_main();
     loop {}
 }
