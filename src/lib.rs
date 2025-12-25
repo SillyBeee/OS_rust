53bb0e7ee897dc03a5ead6d4cf63f6773316891b
SillyBee <3576527475a@gmail.com>
2025-12-25 14:48:04 +0800

    feat: serial和中断处理

---
+++ src/lib.rs
@@ -0,0 +1,93 @@
+#![no_std]
+#![cfg_attr(test, no_main)]
+#![feature(custom_test_frameworks)]
+#![feature(abi_x86_interrupt)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+use core::panic::PanicInfo;
+pub mod serial;
+pub mod vga_buffer;
+pub mod interrupts;
+pub mod gdt;
+
+//特征与泛型
+pub trait Testable {
+    fn run(&self) -> ();
+}
+
+impl<T> Testable for T
+    where
+        T: Fn(),
+{
+    fn run(&self) {
+        serial_print!("{}...\t", core::any::type_name::<T>());
+        self();
+        serial_println!("[ok]");
+    }
+}
+
+
+pub fn test_runner(tests: &[&dyn Testable]) {
+    serial_println!("Running {} tests", tests.len());
+    for test in tests {
+        test.run();
+    }
+    exit_qemu(QemuExitCode::Success);
+}
+
+pub fn test_panic_handler(info: &PanicInfo) -> ! {
+    serial_println!("[failed]\n");
+    serial_println!("Error: {}\n", info);
+    exit_qemu(QemuExitCode::Failed);
+    loop {}
+}
+
+
+//qemu内isa-debug-exit实现的推出
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
+}
+
+//中断描述表初始化
+pub fn init() {
+    gdt::init();
+    interrupts::init_idt();
+}
+
+
+
+
+/// Entry point for `cargo test`
+#[cfg(test)]
+#[unsafe(no_mangle)]
+pub extern "C" fn _start() -> ! {
+    init();
+    test_main();
+    loop {}
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    test_panic_handler(info)
+}
+
+
+#[test_case]
+fn test_breakpoint_exception() {
+    // invoke a breakpoint exception
+    x86_64::instructions::interrupts::int3();
+}
--- src/main.rs
+++ src/main.rs
@@ -1,56 +1,30 @@
 #![no_std] // 不链接 Rust 标准库
 #![no_main] // 禁用所有 Rust 层级的入口点
 #![feature(custom_test_frameworks)]
-#![test_runner(crate::test_runner)]
+#![test_runner(os_rust::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
-mod serial;
-mod vga_buffer;
-
-//qemu内isa-debug-exit实现的推出
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-#[repr(u32)]
-pub enum QemuExitCode {
-    Success = 0x10,
-    Failed = 0x11,
-}
-
-pub fn exit_qemu(exit_code: QemuExitCode) {
-    use x86_64::instructions::port::Port;
-
-    unsafe {
-        let mut port = Port::new(0xf4);
-        port.write(exit_code as u32);
-    }
-}
-
-//单元测试
-#[cfg(test)]
-pub fn test_runner(tests: &[&dyn Fn()]) {
-    println!("Running {} tests", tests.len());
-    for test in tests {
-        test();
-    }
-    exit_qemu(QemuExitCode::Success);
-}
-
-#[test_case]
-fn trivial_assertion() {
-    print!("trivial assertion... ");
-    assert_eq!(1, 1);
-    println!("[ok]");
-}
+use os_rust::println;
 
 
+//panic handler
 
-/// 这个函数将在 panic 时被调用
+//条件编译(非测试)
+#[cfg(not(test))] // new attribute
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
-    println!("{}",info);
+    println!("{}", info);
     loop {}
 }
 
+//条件编译(测试)
+#[cfg(test)]
+#[panic_handler]
+#[warn(non_snake_case)]
+fn panic(info: &PanicInfo) -> ! {
+    os_rust::test_panic_handler(info)
+}
 
 
 
@@ -58,6 +32,8 @@ fn panic(info: &PanicInfo) -> ! {
 #[unsafe(no_mangle)] // 不重整函数名
 pub extern "C" fn _start() -> ! {
     println!("ciallo");
+    os_rust::init();
+    x86_64::instructions::interrupts::int3(); 
 
     #[cfg(test)]
     test_main();
